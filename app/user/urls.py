458a912f54aba46047fde74cd05dc909259500ad
NMShihab <nmshihabislam@gmail.com>
2021-06-02 00:01:46 +0600

    using viewsets add create delete put patch funtionality

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from django.urls import path
-
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+
+router.register("dummy-viewset",views.DummyViewSet,basename="dummy-viewset")
 urlpatterns =[
     path("dummy-view/",views.DummyApiView.as_view()),
+    path("",include(router.urls)),
 ]
