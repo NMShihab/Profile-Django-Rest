17502cff1da94013aa3f886816555f978a0a3c84
NMShihab <nmshihabislam@gmail.com>
2021-06-01 21:59:26 +0600

    add get delete pu pach method

--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.http import request
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 class DummyApiView(APIView):
     """Test API"""
+
+    serializer_class = serializers.DummySerializer
 
     def get(self, request,format=None):
         """Return's list of APIView features"""
@@ -15,7 +19,25 @@ class DummyApiView(APIView):
 
         return Response({"message":"Hello","an_apiview":an_apiview})
 
+    def post(self,request):
+        """Create a hello message with input name"""
+        serializer = self.serializer_class(data=request.data)
 
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f'Hello {name}!'
+            return Response({'message':message})
 
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
 
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
